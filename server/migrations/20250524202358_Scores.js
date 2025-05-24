903f1014a3fb382ff9b270823eb60d120be6a6bf
Vincent Dixon <vincentedward007@gmail.com>
2025-05-24 13:49:14 -0700

    Added Scores table migration

---
+++ server/migrations/20250524202358_Scores.js
@@ -0,0 +1,25 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('scores', table => {
+    table.increments('scores_id').primary
+    table.integer('user_id')
+    table.foreign('user_id').references('userId').inTable('accounts')
+    table.integer('distance')
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.alterTable('scores', table => {
+    table.dropForeign('user_id')
+  })
+  .then(() => {
+    return knex.schema.dropTableIfExists('scores');
+  })
+};
