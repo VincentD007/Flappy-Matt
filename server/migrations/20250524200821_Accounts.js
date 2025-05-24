a06321da48bafdabf3a97c545373a467e418f0e1
Vincent Dixon <vincentedward007@gmail.com>
2025-05-24 13:23:15 -0700

    Added accounts table migration

---
+++ server/migrations/20250524200821_Accounts.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('accounts', table => {
+    table.increments('userId');
+    table.string('username');
+    table.string('password');
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('accounts');
+};
