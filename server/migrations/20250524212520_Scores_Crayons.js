f05cc1313e0dadd504546e77d626f5c8b3ccafcd
Vincent Dixon <vincentedward007@gmail.com>
2025-05-24 17:21:04 -0700

    Added scores_crayons table migration

---
+++ server/migrations/20250524212520_Scores_Crayons.js
@@ -0,0 +1,26 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('scores_crayons', table => {
+    table.increments('scores_crayons_id')
+    table.integer('score_id')
+    table.foreign('score_id').references('scores_id').inTable('scores')
+    table.integer('crayon_id')
+    table.foreign('crayon_id').references('crayonsID').inTable('crayons')
+    table.integer('amount')
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.alterTable('scores_crayons', table => {
+    table.dropForeign('score_id')
+    table.dropForeign('crayon_id')
+  })
+  .then(() => knex.schema.dropTableIfExists('scores_crayons'))
+};
