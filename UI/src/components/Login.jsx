101c1e89d90948137e4bb4117af3a8d3e719c827
Vincent Dixon <vincentedward007@gmail.com>
2025-05-26 21:23:02 -0700

    Fixed Login form submission

--- UI/src/components/Login.jsx
+++ UI/src/components/Login.jsx
@@ -1,21 +1,28 @@
+import { useState, useRef } from 'react';
 import '../styles/Login.css';
 
 export default function Login() {
+    const [NotificationVisible, setVisibility] = useState('true');
+    const notification = useRef('Invalid Credentials');
+
     return (
-        <div id='LoginContainer'>
+        <form id='LoginContainer' onSubmit={(event => {event.preventDefault();})}>
             <h1>Flappy Matt</h1>
             <input 
-            id='UserInput'
+            className='UserInput'
             type='text'
-            required='true'
-            placeholder='Username'></input>
+            required={true}
+            placeholder='Username'/>
             <input 
-            id='UserInput'
+            className='UserInput'
             type='password'
-            required='true'
-            placeholder='Password'></input>
-            <div className='Button'>Login</div>
-            <div className='Button'>Register</div>
-        </div>
+            required={true}
+            placeholder='Password'/>
+            <input type='submit' value='Login' className='Button' onClick={() => {}}/>
+            <input type='submit' value='Register' className='Button'/>
+            <div id='Notification' 
+            style={{visibility: NotificationVisible}}
+            >{notification.current}</div>
+        </form>
     )
 }
